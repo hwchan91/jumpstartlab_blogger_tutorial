242cd328244546112e641f5b63dc1c139e1c334b
hwchan91 <hwchan91@gmail.com>
2017-08-08 18:01:23 +0800

    integer mispelling

--- db/migrate/20170329074704_create_taggings.rb
+++ db/migrate/20170329074704_create_taggings.rb
@@ -1,7 +1,7 @@
 class CreateTaggings < ActiveRecord::Migration[5.0]
   def change
     create_table :taggings do |t|
-      t.intger :tag_id
+      t.integer :tag_id
       t.references :article, foreign_key: true
 
       t.timestamps
